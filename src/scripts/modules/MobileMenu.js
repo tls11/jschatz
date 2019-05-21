82d33894ee0293d2607f2ee136b19a3e9234ed2a
theo <theospielman@gmail.com>
2019-05-21 01:54:39 -0700

    Remove input icons, attribution, base, contact header tag, and extraneous p. Refactor form, and left align labels. Title-case nav links.  Modify nav css and script.

--- src/scripts/modules/MobileMenu.js
+++ src/scripts/modules/MobileMenu.js
@@ -1,26 +1,22 @@
 class MobileMenu {
   constructor() {
+    this.burgerOpen = false;
     this.burgerBox = document.querySelector('.main-nav__burger-box');
     this.menuContent = document.querySelector('.main-nav__menu-content');
-    this.navLinks = document.querySelectorAll('.main-nav__menu-content a');
     this.events();
   }
 
   events() {
     this.burgerBox.addEventListener('click', this.toggleMenu.bind(this));
-
-    this.navLinks.forEach(link => {
-      link.addEventListener('click', () => {
-        if (this.burgerBox.classList.contains('main-nav__burger-box--xed')) {
-          this.toggleMenu();
-        }
-      });
-    });
+    window.addEventListener('click', this.toggleMenu.bind(this));
   }
 
-  toggleMenu() {
-    this.burgerBox.classList.toggle('main-nav__burger-box--xed');
-    this.menuContent.style.maxHeight = this.menuContent.style.maxHeight ? null : this.menuContent.scrollHeight + 20 + 'px';
+  toggleMenu(e) {
+    e.stopPropagation();
+    if (e.currentTarget === window && !this.burgerOpen) return;
+    this.burgerOpen = !this.burgerOpen;
+    this.burgerBox.classList.toggle('main-nav__burger-box--transform');
+    this.menuContent.classList.toggle('opened');
   }
 }
 
